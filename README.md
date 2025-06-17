02ce7af697f39fcfee8151a4d72cded666c53429
dede1751 <63297263+dede1751@users.noreply.github.com>
2025-06-17 11:23:22 +0200

    Basic project setup

---
+++ README.md
@@ -0,0 +1,2 @@
+# Rusterizer
+WIP software rasterizer in Rust inspired by Sebastian Lague's video.
---
+++ engine/src/main.rs
@@ -0,0 +1,16 @@
+use raylib::prelude::*;
+use raster::add;
+
+fn main() {
+    let (mut rl, thread) = raylib::init()
+        .size(640, 480)
+        .title("Hello, World")
+        .build();
+     
+    while !rl.window_should_close() {
+        let mut d = rl.begin_drawing(&thread);
+         
+        d.clear_background(Color::WHITE);
+        d.draw_text(add(2, 2).to_string().as_str(), 12, 12, 20, Color::BLACK);
+    }
+}
